43ad1b02913a06aec507adcdcd871ec016e0f967
TYRONE AVNIT <tyronemichael@gmail.com>
2023-11-21 06:52:23 -0600

    Add working example

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,18 +1,37 @@
-import { build as buildWith } from "@instructor-ts/instructor";
+import { Role, build as buildWith } from "@instructor-ts/instructor";
 import { CompletionsApi } from "@instructor-ts/completions-api";
+import { IsString, IsIn } from "class-validator";
+import { config } from "dotenv";
+import { JSONSchema } from "class-validator-jsonschema";
 
-class UserDetail {
-  constructor(
-    readonly name: string,
-    readonly age: number,
-  ) {}
+config();
+
+class Labels {
+  @IsIn(["SPAM", "NOT_SPAM"])
+  labels?: string;
+
+  @IsString()
+  @JSONSchema({
+    description: "Description why this was classified as SPAM or NOT_SPAM",
+  })
+  description: string;
 }
 
-const instructor = buildWith([new CompletionsApi()]);
+const instructor = buildWith(
+  [new CompletionsApi()],
+  process.env.OPENAI_API_KEY,
+);
+
 const result = instructor.completions.create({
-  responseModel: UserDetail,
-  model: "",
-  messages: [],
+  responseModel: Labels,
+  model: process.env.OPENAI_MODEL,
+  messages: [
+    {
+      role: Role.USER,
+      content:
+        "Classify the following text: Hello, my name is John and I am 20 years old nigerian prince",
+    },
+  ],
 });
 
-console.log(result);
+result.then((r) => console.log(r));
